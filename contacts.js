914058bce1056d95c74eac8191fe56661a3a92ec
oyesa <mercyoyesa2018@gmail.com>
2022-03-30 13:13:38 +0300

    add photos in see more section

--- contacts.js
+++ contacts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#contact-btn").click(function(){
+  $('#contact-btn').click(function(){
     alert('Thank you for getting in touch with the Memory Collector. She will get back to you soon earthling.')
   });
 });
